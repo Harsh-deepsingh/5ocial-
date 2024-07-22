daf2f144b7a9cb76a554f0133771d4d830248bcf
Harsh-deepsingh <153255817+Harsh-deepsingh@users.noreply.github.com>
2024-07-21 22:42:40 -0600

    search route updated

--- app/api/feed/searchPost/route.ts
+++ app/api/feed/searchPost/route.ts
@@ -3,11 +3,23 @@ import { PrismaClient } from "@prisma/client";
 
 const client = new PrismaClient();
 
-export async function GET(req: NextRequest) {
+export async function POST(req: NextRequest) {
   try {
-    const search = req.json();
-    console.log("search");
-    return NextResponse.json("Listing here");
+    const { userInput } = await req.json();
+    const communityId = req.nextUrl.searchParams.get("communityId") ?? "";
+    const searchResult = await client.post.findMany({
+      where: {
+        communityId,
+        content: {
+          contains: userInput,
+        },
+      },
+      select: {
+        content: true,
+      },
+    });
+
+    return NextResponse.json(searchResult);
   } catch (error) {
     console.log(error);
   }
