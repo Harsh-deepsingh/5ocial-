ec1e31f2517eaacefcf3d485fa5a73a723b25a72
Harsh-deepsingh <153255817+Harsh-deepsingh@users.noreply.github.com>
2024-08-11 23:43:03 -0600

    Navbar updated

--- app/components/Navbar/Navbar.tsx
+++ app/components/Navbar/Navbar.tsx
@@ -3,7 +3,7 @@ import SecButton from "../Buttons/SecButton";
 
 const Navbar = () => {
   return (
-    <div className="relative z-10 flex p-3 bg-theme-grey border-b border-theme-border">
+    <div className=" relative z-10 flex p-3  bg-theme-grey border-b border-theme-border">
       <div className="flex items-center  w-full justify-between">
         <p>5ocial</p>
         <div className="flex flex-row">
