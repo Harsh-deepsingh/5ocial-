40d9bd4a9f96514a5e7d7ed7342e6f594bee5161
Harshdeep Singh <153255817+Harsh-deepsingh@users.noreply.github.com>
2024-10-16 14:51:06 -0600

    Update uploadthing.ts

--- app/utils/uploadthing.ts
+++ app/utils/uploadthing.ts
@@ -5,13 +5,5 @@ import {
 
 import type { OurFileRouter } from "../api/uploadthing/core";
 
-// Set the correct URL to your Next.js API route for handling uploads
-const uploadUrl = "https://blindlysocial.com/api/uploadthing";
-
-export const UploadButton = generateUploadButton<OurFileRouter>({
-  url: uploadUrl,
-});
-
-export const UploadDropzone = generateUploadDropzone<OurFileRouter>({
-  url: uploadUrl,
-});
+export const UploadButton = generateUploadButton<OurFileRouter>();
+export const UploadDropzone = generateUploadDropzone<OurFileRouter>();
